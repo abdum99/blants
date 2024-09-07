cf45edb64d4d132584bd31b32da7eb15e9e846c0
abdum99 <a.mohamdy99@gmail.com>
2024-09-06 19:56:30 -0700

    minor refactor

--- blants.py
+++ blants.py
@@ -1,19 +1,21 @@
 from machine import ADC, Pin
-import time
+from time import sleep
 from board import Board, NODEMCU_ESP8266 
 import sys
 # from umqtt.simple import MQTTClient
-import config
 
-MOISTURE_LEVEL_DRY = 58000
-MOISTURE_LEVEL_LIQUID = 30000
+from config import MOISTURE_LEVEL_LIQUID, MOISTURE_LEVEL_DRY, MOISTURE_LEVEL_MIN_THRES, MOISTURE_LEVEL_MAX_THRES 
 
-board = Board(NODEMCU_ESP8266)
+
+def should_water(measurement):
+    return measurement < MOISTURE_LEVEL_MIN_THRES
+
 
 class Blants:
     def __init__(self):
-        self.adc = board.ANALOG_PIN
-        self.led = board.ON_BOARD_LED_PIN
+        self.board = Board(NODEMCU_ESP8266)
+        self.adc = self.board.ANALOG_PIN
+        self.led = self.board.ON_BOARD_LED_PIN
 
     #     self._setup()
         self._do_prechecks()
@@ -41,22 +43,16 @@ class Blants:
 
         return True
 
-
-    def run(self):
-        while True:
-            print("Moisture:", self.measure_moisture(), "%")
-            time.sleep(2)
-
     def measure_moisture(self):
         measurements = []
-        for _ in range(10):
+        for _ in range(5):
             val = self.adc.read_u16()
             print("val:", val)
             measurements.append(val)
             self.led.on()
-            time.sleep(0.5)
+            sleep(0.5)
             self.led.off()
-            time.sleep(0.5)
+            sleep(0.5)
 
         raw_val = sum(measurements) / len(measurements)
 
@@ -64,6 +60,27 @@ class Blants:
             return MOISTURE_LEVEL_LIQUID
         return 1 - ((raw_val - MOISTURE_LEVEL_LIQUID) / (MOISTURE_LEVEL_DRY - MOISTURE_LEVEL_LIQUID))
 
+
+    def _water(self):
+        while self.measure_moisture() < MOISTURE_LEVEL_MAX_THRES:
+            print("watering; water pump on")
+            sleep(1)
+            print("water pump off")
+            sleep(config.WATER_PUMP_SLEEP_PERIOD)
+
+
+    def run(self):
+        while True:
+            measurement = self.measure_moisture()
+            print("Moisture:", measurement * 100, "%")
+            if should_water(measurement):
+                print("watering...")
+                self._water()
+                print("done watering")
+                continue
+
+            print("going into deep sleep")
+            sleep(config.BLANTS_SLEEP_PERIOD)
 
 if __name__ == '__main__':
     blants = Blants()
--- main.py
+++ main.py
@@ -1,47 +1,6 @@
-import network
-import usocket as socket
-
-import config
 import blants
 
-# debug
-import esp
-esp.osdebug(None)
-
-import gc
-gc.collect()
-
-def setup_wifi():
-# set access point
-    ap = network.WLAN(network.AP_IF) # create access-point interface
-    ap.active(True)         # activate the interface
-    ap.config(essid=config.WIFI_SSID, password=config.WIFI_PASSWORD) # set the SSID of the access point
-    # wait for it to start
-    while ap.active() == False:
-        pass
-
-    print("wifi up. addr:", ap.ifconfig())
-
-def setup_server():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # timeout in 5 minutes
-    sock.settimeout(300) 
-    sock.bind(('', 80))
-    print("socket bound to:", )
-    sock.listen(5)
-
-    conn, addr = sock.accept()
-    print("got connection from", str(addr))
-
-    request = conn.recv(1024)
-    print("received:", request)
-
-
 if __name__ == '__main__':
-    # network setup
-    setup_wifi()
-    # setup_server()
-
     # blants setup
     blant = blants.Blants()
     blant.run()

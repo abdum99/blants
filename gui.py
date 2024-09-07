7bf71ffa9cd6976272df51ea16ed76747954f7a6
abdum99 <a.mohamdy99@gmail.com>
2024-09-06 19:53:22 -0700

    graph

--- gui.py
+++ gui.py
@@ -20,6 +20,13 @@ class Emoji:
     def Sleepy():
         return Emoji("sleepy.dat")
 
+
+def map_point(val, s_min, s_max, t_min, t_max):
+    return int(((val - s_min) / (s_max - s_min)) * (t_max - t_min)) + t_min
+
+COLOR_BLACK = 0x0
+COLOR_WHITE = 0xf
+
 # y addr's start at 6 bc of memory layout
 Y_START = 6
 TOP_BAR_TEXT_START_X = 5
@@ -28,45 +35,76 @@ TOP_BAR_LINE_Y = 20
 EMOJI_START_X = 5
 EMOJI_START_Y = 40
 TEXT_START_X = 150
-TEXT_MAX_WIDTH
-TEXT_MAX_HEIGHT = 50
+GRAPH_START_X = 150
+GRAPH_END_X = 290
+GRAPH_START_Y = 35
+GRAPH_END_Y = 115
 class BlantsGui:
     def __init__(self, disp: Display):
         self.disp = disp
 
+    def _emoji(self, em):
+        if not isinstance(em, Emoji):
+            raise Exception("bad emoji")
+
+        with open(em.data, 'rb') as em_bytes:
+            em_buf = framebuf.FrameBuffer(bytearray(em_bytes.read()), 128, 80, framebuf.MONO_HLSB)
+            self.disp.blit(em_buf, EMOJI_START_X, EMOJI_START_Y)
+
+    def _graph(self, points, label=None, display_last=True, min=0, max=100):
+        graph_width = GRAPH_END_X - GRAPH_START_X
+        graph_height = GRAPH_END_Y - GRAPH_START_Y
+
+        # clear area
+        self.disp.rect(GRAPH_START_X, GRAPH_START_Y, graph_width, graph_height, COLOR_WHITE, True)
+
+        # borders
+        self.disp.rect(GRAPH_START_X, GRAPH_START_Y, graph_width, graph_height, COLOR_BLACK)
+
+        # # draw axis
+        # self.disp.vline(GRAPH_START_X, GRAPH_START_Y, graph_height, COLOR_BLACK)
+
+        # draw points
+        x_inc = graph_width // len(points)
+        x = GRAPH_START_X
+        for i in range(len(points) - 1):
+            self.disp.line(
+                x,
+                #TODO: correct this by flipping graph_start and graph_end
+                map_point(points[i], min, max, GRAPH_END_Y, GRAPH_START_Y),
+                x + x_inc,
+                map_point(points[i + 1], min, max, GRAPH_END_Y, GRAPH_START_Y),
+                COLOR_BLACK
+            )
+            x += x_inc
+
+        if display_last:
+            self.disp.rect(GRAPH_END_X - 20, GRAPH_END_Y - 20, 20, 20, COLOR_BLACK, True)
+            self.disp.text(str(points[-1]), GRAPH_END_X - 18, GRAPH_END_Y - 14, COLOR_WHITE)
+
+        # label
+        if label != None:
+            # horizontal centering
+            # assuming each letter is 8 pixels
+            # num_letters * 8 / 2
+            self.disp.text(label, GRAPH_START_X + (graph_width // 2) - (len(label) * 4), GRAPH_END_Y + 5, COLOR_BLACK)
+
     # TODO: Add more stuff here (e.g. widget to display moisture in a corner somewhere)
     def render(
         self,
-        text,
+        topbar=None,
         emoji=None,
-        topbar=None
+        data_points=None,
+        graph_label=None,
     ):
-        lines = text.splitlines()
-        if len(lines) > 5:
-            raise Exception("too many lines")
-
-        # display text
-        y_padding = 0
-        for l in lines:
-            if len(l) > 16:
-                raise Exception("line too long")
-
-            self.disp.text(l, TEXT_START_X, TEXT_START_Y + y_padding, 0x00)
-
-            y_padding += 10
-
         if emoji != None:
-            if not isinstance(emoji, Emoji):
-                raise Exception("bad emoji")
-
-            with open(emoji.data, 'rb') as em_bytes:
-                print("reading emoji data for", emoji, emoji.data)
-                em_buf = framebuf.FrameBuffer(bytearray(em_bytes.read()), 128, 80, framebuf.MONO_HLSB)
-                print("read", em_buf)
-                self.disp.blit(em_buf, EMOJI_START_X, EMOJI_START_Y)
+            self._emoji(emoji)
 
         if topbar != None:
-            self.disp.hline(0, TOP_BAR_LINE_Y, self.disp.dims()[1] - 1, 0x00)
-            self.disp.text(topbar, TOP_BAR_TEXT_START_X, TOP_BAR_TEXT_Y, 0x00)
+            self.disp.hline(0, TOP_BAR_LINE_Y, self.disp.dims()[1] - 1, COLOR_BLACK)
+            self.disp.text(topbar, TOP_BAR_TEXT_START_X, TOP_BAR_TEXT_Y, COLOR_BLACK)
+
+        if data_points != None:
+            self._graph(data_points, label=graph_label)
 
         self.disp.show()
---
+++ test_gui.py
@@ -0,0 +1,31 @@
+from board import Board, NODEMCU_ESP32
+from display import Epd2in9Display
+from gui import BlantsGui, Emoji
+from time import sleep
+
+sample_points = [24, 22, 12, 81, 75, 60, 40, 9, 72, 50, 33, 28, 10, 5, 99, 90, 89, 88, 39, 38, 37, 36, 35, 34]
+sample_points_2 = [40, 9, 72, 50, 33, 28, 10, 5, 99, 90, 89, 88, 39, 38, 37, 36, 35, 34, 24, 22, 12, 81, 75, 60]
+
+if __name__ == '__main__':
+    board = Board(NODEMCU_ESP32)
+    epd_disp = Epd2in9Display(board)
+    gui = BlantsGui(epd_disp)
+
+    print("ready about to render \"hello world\"")
+    print("about to render")
+    # gui.render(
+    #     topbar="moisture: 26.3%",
+    #     emoji=Emoji.Confused(),
+    # )
+    # sleep(5)
+    # gui.render(
+    #     emoji=Emoji.Good(),
+    #     data_points=sample_points,
+    # )
+    sleep(5)
+    gui.render(
+        emoji=Emoji.Sleepy(),
+        topbar="sleeping since 8:12pm 8/31/2024",
+        data_points=sample_points_2,
+        graph_label="Last 24 hours",
+    )
